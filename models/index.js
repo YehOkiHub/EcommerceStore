f265163a1905f7ac47da36b7782473d8b80d2d0e
Jeff <jeff077872@hotmail.com>
2022-12-08 20:07:22 -0500

    deleted some unecessary starter code files

--- models/index.js
+++ models/index.js
@@ -1,23 +1,17 @@
 // import models
 const Product = require('./Product');
-const Category = require('./Category');
+
 
 
 // Products belongsTo Category
-Product.belongsTo(Category,{
-    foreignKey: "category_id",
-    onDelete: "CASCADE"
 
-}),
 
 // Categories have many Products
-Category.hasMany(Product,{
-  foreignKey: "category_id"
-})
+
 
 // Products belongToMany Tags (through ProductTag)
 // Tags belongToMany Products (through ProductTag)
 module.exports = {
-  Product,
-  Category
+  Product
+  
 };
