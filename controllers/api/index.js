424d8a423a84eb760bda016644df9f2951ddc4b6
Jeff <jeff077872@hotmail.com>
2022-12-19 18:24:24 -0500

    trying this

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const productRoutes = require('./product-routes');
-const user = require('./user');
+const user = require('./user.js');
 
 router.use('/products', productRoutes);
 router.use('/users', user);
