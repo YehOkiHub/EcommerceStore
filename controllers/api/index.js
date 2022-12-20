685d72fec56b2d64f6efc9e34d061e70e8d65ff8
Jeff <jeff077872@hotmail.com>
2022-12-19 21:15:02 -0500

    test

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const productRoutes = require('./product-routes');
-const user = require('./user.js');
+const user = require('./user-routes.js');
 
 router.use('/products', productRoutes);
 router.use('/users', user);
